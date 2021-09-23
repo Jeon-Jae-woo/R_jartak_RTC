e206b39d58f4e91206150c7be49f2575326fa44a
Jeon-Jae-woo <jaewoo6836@naver.com>
2021-09-23 10:35:40 +0900

    host

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-let socket = io.connect("http://localhost:3000");
+let socket = io.connect();
 let divVideoChatLobby = document.getElementById("video-chat-lobby");
 let divVideoChat = document.getElementById("video-chat-room");
 let joinButton = document.getElementById("join");
